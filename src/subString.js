ffa348104ac4b227f785220ff37f4b75df2cf0f8
Akash Bhosale <MY_NAME@example.com>
2022-09-21 12:43:49 +0530

    ...

--- src/subString.js
+++ src/subString.js
@@ -2,5 +2,8 @@ export default function lengthOfLongestSubstring(str) {
   let longest = '';
   for (let i = 0; i < str.length; i++)
     if (longest.includes(str[i]) === false) longest += str[i];
-  return longest.length;
+  return {
+    longest,
+    length: longest.length,
+  };
 }
--- src/subString.test.js
+++ src/subString.test.js
@@ -2,16 +2,22 @@ import { expect, it } from 'vitest';
 import lengthOfLongestSubstring from './subString';
 
 it('should get the substring', () => {
-  let res = lengthOfLongestSubstring('abcabcbb');
+  let res = lengthOfLongestSubstring('abcabcbb').length;
+  let substring = lengthOfLongestSubstring('abcabcbb').longest;
+  expect(substring).toBe('abc');
   expect(res).toBe(3);
 });
 
 it('should get the substring', () => {
-  let res = lengthOfLongestSubstring('bbbbb');
+  let res = lengthOfLongestSubstring('bbbbb').length;
+  let substring = lengthOfLongestSubstring('bbbbb').longest;
+  expect(substring).toBe('b');
   expect(res).toBe(1);
 });
 
 it('should get the substring', () => {
-  let res = lengthOfLongestSubstring('pwwkew');
+  let res = lengthOfLongestSubstring('pwwkew').length;
+  let substring = lengthOfLongestSubstring('pwwkew').longest;
+  expect(substring).toBe('pwke');
   expect(res).toBe(4);
 });
