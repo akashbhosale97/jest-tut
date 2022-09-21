f02c0b39e5d9d8fb68f07f123679b979c1584957
Akash Bhosale <MY_NAME@example.com>
2022-09-21 12:44:17 +0530

    ..

--- src/subString.test.js
+++ src/subString.test.js
@@ -2,22 +2,22 @@ import { expect, it } from 'vitest';
 import lengthOfLongestSubstring from './subString';
 
 it('should get the substring', () => {
-  let res = lengthOfLongestSubstring('abcabcbb').length;
+  let length = lengthOfLongestSubstring('abcabcbb').length;
   let substring = lengthOfLongestSubstring('abcabcbb').longest;
   expect(substring).toBe('abc');
-  expect(res).toBe(3);
+  expect(length).toBe(3);
 });
 
 it('should get the substring', () => {
-  let res = lengthOfLongestSubstring('bbbbb').length;
+  let length = lengthOfLongestSubstring('bbbbb').length;
   let substring = lengthOfLongestSubstring('bbbbb').longest;
   expect(substring).toBe('b');
-  expect(res).toBe(1);
+  expect(length).toBe(1);
 });
 
 it('should get the substring', () => {
-  let res = lengthOfLongestSubstring('pwwkew').length;
+  let length = lengthOfLongestSubstring('pwwkew').length;
   let substring = lengthOfLongestSubstring('pwwkew').longest;
   expect(substring).toBe('pwke');
-  expect(res).toBe(4);
+  expect(length).toBe(4);
 });
